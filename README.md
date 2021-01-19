4d72430c5ac31b67d881d191dd4b79625b7fce81
Lukasz <lukasz.gras.97@gmail.com>
2021-01-19 15:15:34 +0100

    Initial config

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-serverTS
+What i learned:<br>
+~express,<br>
+~typescript,<br>
+~passsport-jwt,<br>
+~validation data,<br>
+~work with mongodb<br>
+~routing,
---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cors = require('cors');
+
+//const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Cors
+app.use(cors());
+
+// DB Config
+// const db = require("./config/keys").mongoURI;
+//
+// // Connect to MongoDB
+// mongoose.connect(db, { useNewUrlParser: true,useUnifiedTopology: true})
+//     .then(() => console.log("MongoDB successfully connected"))
+//     .catch((err:never) => {
+//         throw new Error(err)
+//     })
+// ;
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+//require("./config/passport")(passport);
+app.get("/api/users",(req,res) => {
+   res.send("Hi");
+});
+// Routes
+//app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
