b3d9553118164b8ac600c0698add6a35b5037ab8
Kloosk <lukasz.gras.97@gmail.com>
2021-03-30 16:11:16 +0200

    Docs

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -26,5 +35,26 @@ require("./config/passport")(passport_1.default);
 app.use("/api/users", users);
 app.use("/api/", publicNotes);
 app.use("/api/notes", notes);
+app.get('/newmember', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const token = req.header('Authorization');
+    if (token) {
+        try {
+            return res.json(["raz", "dwa", "trzy"]);
+        }
+        catch (e) {
+            res.status(401).send('Incorrect token');
+        }
+    }
+    else {
+        res.status(401).send('Not authorized');
+    }
+}));
+app.post('/auth', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    if (!email || !password)
+        return res.status(400).send('Did not supply either email or password!');
+    const accessToken = 1234567891011121316466;
+    return res.json({ accessToken });
+}));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
